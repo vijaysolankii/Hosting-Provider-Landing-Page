817f2adfe62a0589d8043fc36606fa9c36b771eb
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-11-19 00:01:51 +0530

    added files

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = ({headContent,paragraphContent,boldContent}) => {
+  return (
+    <div className="heading">
+        <h1></h1>
+    </div>
+  )
+}
+
+export default Heading
--- src/sections/HeroBanner.jsx
+++ src/sections/HeroBanner.jsx
@@ -1,1 +1,27 @@
+import React from 'react'
+import Button from '../components/Button'
 
+const HeroBanner = () => {
+  return (
+    <section className='banner'>
+        <div className="container">
+            <div className="row">
+                <div className="banner-content w-6/12">
+                    <h1>The best and <strong>chepest</strong> hosting for your website</h1>
+                    <p>Hosting your website here is easy. access it fast, server security is very well maintained. and of course supported by reliable servers.</p>
+                    <div className="btnWrap">
+                        <Button text="login" />
+                    </div>
+                </div>
+                <div className="banner-img w-6/12">
+                    <figure>
+                        <img src="" alt="" />
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroBanner
