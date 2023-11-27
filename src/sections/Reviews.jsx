269a65138fd9ba93c983ac3bac9bcb6834a66c9c
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-11-27 23:43:27 +0530

    added heading info in review component

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -3,12 +3,14 @@ import ReviewSlider from "../components/ReviewSlider";
 import Heading from "../components/Heading";
 
 const Reviews = () => {
-  const content ="what do out customer says?"
+  const content ="what do out customer says?",
+  longContent='You will be surprised how our customers comment on what we do so well. because we really prioritize the satisfection of our customers'
+
   return (
 
     <section className="reviews">
       <div className="container">
-        <Heading headContent={content} />
+        <Heading headContent={content} paragraphContent={longContent} />
         <ReviewSlider />
       </div>
     </section>
