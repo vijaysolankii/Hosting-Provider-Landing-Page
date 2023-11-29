f07357d501b42b05b8c5860ae9cf3723c99acb23
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-11-29 23:35:55 +0530

    added review items data

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -3,13 +3,49 @@ import ReviewSlider from "../components/ReviewSlider";
 import Heading from "../components/Heading";
 
 const Reviews = () => {
-  const content ="what do out customer says?",
-  longContent='You will be surprised how our customers comment on what we do so well. because we really prioritize the satisfection of our customers'
+  const content = "what do out customer says?",
+    longContent =
+      "You will be surprised how our customers comment on what we do so well. because we really prioritize the satisfection of our customers";
 
-  const data = {}
+  const data = [
+    {
+      name: "Slamet Wijaya",
+      designation: "CEO Tokped",
+      review:
+        "Web hosting that i highly recommand because the service provided is very good, hosting is fast so with a pretty fantastic speed",
+      stars: 5,
+    },
+    {
+      name: "Slamet Wijaya",
+      designation: "CEO Tokped",
+      review:
+        "Web hosting that i highly recommand because the service provided is very good, hosting is fast so with a pretty fantastic speed",
+      stars: 5,
+    },
+    {
+      name: "Slamet Wijaya",
+      designation: "CEO Tokped",
+      review:
+        "Web hosting that i highly recommand because the service provided is very good, hosting is fast so with a pretty fantastic speed",
+      stars: 5,
+    },
+    {
+      name: "Slamet Wijaya",
+      designation: "CEO Tokped",
+      review:
+        "Web hosting that i highly recommand because the service provided is very good, hosting is fast so with a pretty fantastic speed",
+      stars: 5,
+    },
+    {
+      name: "Slamet Wijaya",
+      designation: "CEO Tokped",
+      review:
+        "Web hosting that i highly recommand because the service provided is very good, hosting is fast so with a pretty fantastic speed",
+      stars: 5,
+    },
+  ];
 
   return (
-
     <section className="reviews">
       <div className="container">
         <Heading headContent={content} paragraphContent={longContent} />
