4357951f9f81a2b795eb5692625d3fc818c17a74
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-11-30 23:13:58 +0530

    added dynamic data in review slider

--- src/components/ReviewSlider.jsx
+++ src/components/ReviewSlider.jsx
@@ -6,31 +6,33 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/scrollbar";
 
+import Quote from '../assets/images/quotes.svg'
+
 // import required modules
 import { Scrollbar } from "swiper/modules";
 
-const ReviewSlider = () => {
+const ReviewSlider = ({ data }) => {
   return (
     <Swiper
-        scrollbar={{
-          hide: true,
-        }}
-        modules={[Scrollbar]}
-        slidesPerView={1.4}
-        spaceBetween={30}
-        className="reviewSlider bg-white px-5 py-16"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-  )
-}
+      scrollbar={{
+        hide: true,
+      }}
+      modules={[Scrollbar]}
+      slidesPerView={1.4}
+      spaceBetween={30}
+      className="reviewSlider bg-white px-5 py-16"
+    >
+      {data.map((item, _) => (
+        <SwiperSlide key={_}>
+          <i><img src={Quote} alt="quote" /></i>
+          <h5>{data && data.name}</h5>
+          <span>{data && data.designation}</span>
+          <p>{data && data.review}</p>
+          <p>{data && data.stars}</p>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
-export default ReviewSlider
+export default ReviewSlider;
