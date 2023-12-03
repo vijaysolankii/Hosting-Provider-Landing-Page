859f55937409cefd0fd0faeb4a5b91f4d879674e
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-12-03 22:05:22 +0530

    fix files sizeing issue

--- src/utilities/Footer.jsx
+++ src/utilities/Footer.jsx
@@ -19,49 +19,27 @@ const Footer = () => {
             <ul>
               
               <li>Features</li>
-              <li>
-                <a href="#">Domain</a>
-              </li>
-              <li>
-                <a href="#">hosting</a>
-              </li>
-              <li>
-                <a href="#">wordpress</a>
-              </li>
-              <li>
-                <a href="#">web security</a>
-              </li>
+              <li> <a href="#">Domain</a> </li>
+              <li> <a href="#">hosting</a> </li>
+              <li> <a href="#">wordpress</a> </li>
+              <li> <a href="#">web security</a> </li>
             </ul>
           </div>
           <div className="footer-nav-block">
             <ul>
               <li>Help</li>
-              <li>
-                <a href="#">Payment methods</a>
-              </li>
-              <li>
-                <a href="#">know ledgebase</a>
-              </li>
-              <li>
-                <a href="#">Tutorials</a>
-              </li>
+              <li> <a href="#">Payment methods</a> </li>
+              <li> <a href="#">know ledgebase</a> </li>
+              <li> <a href="#">Tutorials</a> </li>
             </ul>
           </div>
           <div className="footer-nav-block">
             <ul>
               <li>social media</li>
-              <li>
-                <a href="#">instagram</a>
-              </li>
-              <li>
-                <a href="#">Twitter</a>
-              </li>
-              <li>
-                <a href="#">facebook</a>
-              </li>
-              <li>
-                <a href="#">youtube</a>
-              </li>
+              <li> <a href="#">instagram</a> </li>
+              <li> <a href="#">Twitter</a> </li>
+              <li> <a href="#">facebook</a> </li>
+              <li> <a href="#">youtube</a> </li>
             </ul>
           </div>
         </div>
