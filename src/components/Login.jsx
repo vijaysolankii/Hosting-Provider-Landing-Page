b1f8c4d885788c06ffba11ab779d54cdac266ccf
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-12-05 01:54:35 +0530

    added login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className="login">
+        <div className="container">
+            <div className="login-form">
+                <input type="text" name="username" id="username" />
+                <input type="password" name="password" id="password" />
+                <input type="submit" value="Login" />
+            </div>
+        </div>
+    </section>
   )
 }
 
