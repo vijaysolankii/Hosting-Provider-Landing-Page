389078614e4d07428e354147f92d6c2b0f0d54e8
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-12-07 01:12:02 +0530

    added 404 page styling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div className='h-[100vh] w-[100vh] flex items-center justify-center md:text-5xl text-2xl text-cyan bg-black-lite text-white'>404 Page Not Found</div>
+    <div className='h-[100vh]   w-[100vh] flex items-center justify-center md:text-5xl text-2xl text-cyan bg-black-lite text-white'>404 Page Not Found</div>
   )
 }
 
