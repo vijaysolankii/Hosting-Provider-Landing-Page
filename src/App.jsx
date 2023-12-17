52292a38d282a5429c1be8da9b4dcb0bf073b97f
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-12-17 23:08:37 +0530

    added smooth scroll in project2 hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import Routing from './utilities/Routing'
-
+import Routing from "./utilities/Routing";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
-  
+  const scrollContainerRef = useRef(null);
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollContainerRef.current,
+      smooth: true,
+      // Add other Locomotive Scroll options as needed
+    });
+
+    // Destroy Locomotive Scroll on component unmount
+    return () => {
+      if (scroll) {
+        scroll.destroy();
+      }
+    };
+  }, []);
   return (
-    <>
-    <Routing />
-    </>
-  )
+    <div ref={scrollContainerRef} data-scroll-container>
+      <Routing />
+    </div>
+  );
 }
 
-export default App
+export default App;
