d384c1c578a5fb73840ce66beeb23e773b71f798
vijaysolanki <vijay_solanki@gecg28.ac.in>
2023-12-20 23:22:17 +0530

    added observer

--- src/sections/HeroBanner.jsx
+++ src/sections/HeroBanner.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import Button from '../components/Button'
-import BannerImg from '../assets/images/bannerImg.svg'
+import React from "react";
+import Button from "../components/Button";
+import BannerImg from "../assets/images/bannerImg.svg";
+import useIntersectionObserver from '../components/useIntersectionObserver'
 
 const HeroBanner = () => {
+  const [ref, isVisible] = useIntersectionObserver();
   return (
-    <section className='banner'>
-        <div className="container">
-            <div className="banner-wrap">
-                <div className="banner-content">
-                    <h1>The best and <i>chepest</i> hosting for your website</h1>
-                    <p>Hosting your website here is easy. access it fast, server security is very well maintained. and of course supported by reliable servers.</p>
-                    <div className="btnWrap">
-                        <Button text="Get Started" classes="btn-primary" />
-                        <Button text="Try Start free" classes="btn-secondary" />
-                    </div>
-                </div>
-                <div className="banner-img">
-                    <figure>
-                        <img src={BannerImg} alt="BannerImg" />
-                    </figure>
-                </div>
+    <section className={`banner ${isVisible ? 'visible' : ''}`} ref={ref}>
+      <div className="container">
+        <div className="banner-wrap">
+          <div className="banner-content">
+            <h1>
+              The best and <i>chepest</i> hosting for your website
+            </h1>
+            <p>
+              Hosting your website here is easy. access it fast, server security
+              is very well maintained. and of course supported by reliable
+              servers.
+            </p>
+            <div className="btnWrap">
+              <Button text="Get Started" classes="btn-primary" />
+              <Button text="Try Start free" classes="btn-secondary" />
             </div>
+          </div>
+          <div className="banner-img">
+            <figure>
+              <img src={BannerImg} alt="BannerImg" />
+            </figure>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
